7440d8fd9ce930c32452685c6256c551d1a5cdd8
Neil <dreyworks23@gmail.com>
2025-05-24 03:13:01 +0800

    location migration creation

--- backend/user/models/__init__.py
+++ backend/user/models/__init__.py
@@ -14,3 +14,4 @@ from .payroll_period import PayrollPeriod
 from .payslip import Payslip
 from .user_salary import UserSalary
 from .benefits import BenefitsConfiguration
+from .location import Location
