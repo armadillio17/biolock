11a2455d997b097fc98acdadb482c2299ae94d4c
kenneth <carredo.kenneth@gmail.com>
2025-06-01 14:59:17 +0800

    remane firebase file

--- backend/user/apps.py
+++ backend/user/apps.py
@@ -13,6 +13,6 @@ class UserConfig(AppConfig):
         
         if not firebase_admin._apps:
             cred = credentials.Certificate(
-                os.path.join(os.path.dirname(__file__), './biolock-a6aa5-firebase-adminsdk-fbsvc-8a6e3b1d3f.json')
+                os.path.join(os.path.dirname(__file__), './biolock.json')
             )
             firebase_admin.initialize_app(cred)
