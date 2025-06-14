640e10b32626aad8ff366398f055e1aa7e9e3586
jerven <latayada1233@gmail.com>
2025-06-14 18:49:01 +0800

    holiday, custom-holiday, payslip, attendance model

--- frontend/src/components/UserSalary.tsx
+++ frontend/src/components/UserSalary.tsx
@@ -347,7 +347,6 @@ export default function UserSalary() {
                     <td className="px-6 py-4 text-sm text-right text-gray-800 whitespace-nowrap">
                       {editingId === salary.id ? (
                         <div className="relative max-w-xs mx-auto">
-                          <Calendar className="absolute w-5 h-5 text-gray-400 left-3 top-2" />
                           <input
                             type="date"
                             className="w-full border border-gray-300 rounded-lg pl-9 focus:ring-2 focus:ring-indigo-400"
