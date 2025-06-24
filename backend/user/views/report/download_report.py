8ef3326d04093d2b63ab77ecc9cfe35124114ab0
kenneth <carredo.kenneth@gmail.com>
2025-06-24 23:38:09 +0800

    fix report error

--- backend/user/views/report/download_report.py
+++ backend/user/views/report/download_report.py
@@ -34,7 +34,7 @@ class DownloadAttendancePDF(APIView):
             # Group attendance by user
             user_attendance_map = defaultdict(list)
             for attendance in attendances:
-                user_attendance_map[attendance['user_id']].append(attendance)
+                user_attendance_map[attendances['user']].append(attendance)
 
             # Determine the full date range of the report
             all_report_dates = [parser.parse(r['date']).date() for r in attendances if 'date' in r]
