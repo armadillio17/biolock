7d2d8f97fe9b95d83430305ece4712221288d696
Neil <dreyworks23@gmail.com>
2025-06-25 02:51:53 +0800

    removed ternary and make the storage url absolute

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 export const base_url = import.meta.env.VITE_API_URL;
-export const storage_url = import.meta.env.VITE_STORAGE_URL || base_url.replace('/api', '');
+export const storage_url = import.meta.env.VITE_STORAGE_URL;
 // export const base_url = "https://biolock.astrosail.site/api";
